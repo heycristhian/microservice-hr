17252af63349bd8171d88de236f30df7e1e661ec
Cristhian Dias <heycristhian@pop-os.localdomain>
2021-04-12 21:02:58 -0300

    Eureka Server criado

---
+++ hr-eureka-server/src/test/java/com/heycristhian/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heycristhian.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
