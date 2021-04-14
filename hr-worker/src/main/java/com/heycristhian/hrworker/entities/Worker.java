e801bf5b1ac501067f76f5a18436e1d55b118fc8
Cristhian Dias <heycristhian@pop-os.localdomain>
2021-04-13 21:16:48 -0300

    Configuring Zuul Api-Gateway

--- hr-worker/src/main/java/com/heycristhian/hrworker/entities/Worker.java
+++ hr-worker/src/main/java/com/heycristhian/hrworker/entities/Worker.java
@@ -74,8 +74,8 @@ public class Worker implements Serializable{
 		this.dailyIncome = dailyIncome;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public Long getId() {
+		return this.id;
 	}
 	
 	
