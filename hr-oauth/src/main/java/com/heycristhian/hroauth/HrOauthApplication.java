dd59879ff65fedae8cf902fce98797fb8a110607
heycristhian <heycristhian@gmail.com>
2021-04-19 22:32:35 -0300

    Oauth created and configured

---
+++ hr-oauth/src/main/java/com/heycristhian/hroauth/HrOauthApplication.java
@@ -0,0 +1,15 @@
+package com.heycristhian.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class HrOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrOauthApplication.class, args);
+	}
+
+}
