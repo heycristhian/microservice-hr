772fee330fb1517a58cd60626c308fc4d4b0b959
heycristhian <heycristhian@gmail.com>
2021-04-21 00:43:15 -0300

    Login, JWT Token

--- hr-oauth/src/main/java/com/heycristhian/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/heycristhian/hroauth/services/UserService.java
@@ -3,21 +3,34 @@ package com.heycristhian.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.heycristhian.hroauth.entities.User;
 import com.heycristhian.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
-	
+public class UserService implements UserDetailsService {
+
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
-	
+
 	@Autowired
 	private UserFeignClient userFeignClient;
-	
+
 	public User findByEmail(String email) {
 		User user = userFeignClient.findByEmail(email).getBody();
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException("User not found");
+		}
 		return user;
 	}
 }
